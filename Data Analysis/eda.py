3d6b19337731d4ee594036bf7813c6932923e2c6
Colin Watanabe <cwat530@aucklanduni.ac.nz>
2021-04-17 18:41:06 +1200

    Added processed file with DEM

---
+++ Data Analysis/eda.py
@@ -0,0 +1,41 @@
+import rasterio
+from rasterio.merge import merge
+import os
+import glob
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
+PLOT = True
+MAXHEIGHT = 3
+dirpath = "Data\\lds-bay-of-plenty-lidar-1m-dsm-2018-2019-GTiff\\"
+out_fp = "Data\\processed\\DEM_edgecombe.tiff"
+fns = os.path.join(dirpath,"*.tif")
+
+fps = glob.glob(fns)
+
+data_files= []
+for fp in fps:
+    data = rasterio.open(fp)
+    data_files.append(data)
+    #dems = rasterio.open(fps)
+
+mosaic, out_trans = merge(data_files)
+out_meta = data.meta.copy()
+
+# Update the metadata
+out_meta.update({"driver": "GTiff",
+                     "height": mosaic.shape[1],
+                     "width": mosaic.shape[2],
+                     "transform": out_trans,
+                     "crs": "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs "
+                     }
+                    )
+with rasterio.open(out_fp, "w", **out_meta) as dest:
+    dest.write(mosaic)
+
+if PLOT:
+    mask = mosaic[0,:,:] > MAXHEIGHT
+    mosaic[0,mask] = MAXHEIGHT
+    sns.heatmap(mosaic[0,:,:], center=0)
+    plt.show()
+
