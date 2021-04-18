bdf037dcc0d2ee345387f339526c0151391f40fe
Jason Ko <jekb2019@gmail.com>
2021-04-18 12:24:48 +1200

    push before adding vehicle selection

--- frontend/service/kepler.js
+++ frontend/service/kepler.js
@@ -5,17 +5,26 @@ const parent = window.parent;
 
 
 
-//receive
+//receive when iframe is c hanged
 window.addEventListener('message', function(e) {
     let view = e.data;
 
     if(view === "map") {
       viewMap(KeplerGl, store);
+      sendMessageToParent(view);
     } else if(view === "satellite") {
       viewSatellite(KeplerGl, store);
+      sendMessageToParent(view);
     } else if(view === "road") {
-        console.log(" r - r");
+      // view road
+      sendMessageToParent(view);
     } else {
         throw new Error("invalid view");
     }
 });
+
+// send message to iframe when view is changed
+function sendMessageToParent(view) {
+  parent.postMessage(view, "*");
+  console.log("view changed");
+}
